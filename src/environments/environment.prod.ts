6667799bfd537f3dda8ee49761e0c7b5b3f0bdcf
Christopher Alexander Aedo Pizarro <Christopher.aedo.p@gmail.com>
2017-10-08 19:35:13 -0300

    primera version -2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: 'AIzaSyB9bpfQKwfw7y6Ui9fLbXsZGMII_m3Wdqg',
-    authDomain: 'firechat-c5378.firebaseapp.com',
     databaseURL: 'https://firechat-c5378.firebaseio.com',
     projectId: 'firechat-c5378',
     storageBucket: 'firechat-c5378.appspot.com',
